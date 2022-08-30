2efe369f2c86deb61c8d217fb732fdd613b985d8
Juan Sebastian Herrera Gomez <jsebastianhg1006@gmail.com>
2022-08-30 17:13:36 -0500

    Rename directories

--- src/exercises_part1_cap3/Propuesto.java
+++ src/exercises_part1_cap3/Propuesto.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package exercises_cap3;
+package exercises_part1_cap3;
 
 import java.util.Scanner;
 
--- src/exercises_part1_cap4/Resuelto.java
+++ src/exercises_part1_cap4/Resuelto.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package exercises_cap4;
+package exercises_part1_cap4;
 
 import java.util.Scanner;
 
